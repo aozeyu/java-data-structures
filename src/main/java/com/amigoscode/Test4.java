e5492cd7b74ef2f387185befabb909a4107e8794
aozeyu <3484766443@qq.com>
2023-10-05 15:53:58 +0800

    add code

---
+++ src/main/java/com/amigoscode/Test4.java
@@ -0,0 +1,20 @@
+package com.amigoscode;
+
+public class Test4 {
+  public static void main(String[] args) {
+    study();
+  }
+
+  public static void eat() {
+    System.out.println("吃饭");
+  }
+  public static void sleep() {
+    System.out.println("睡觉");
+  }
+
+  public static void study() {
+    eat();
+    System.out.println("学习");
+    sleep();
+  }
+}
